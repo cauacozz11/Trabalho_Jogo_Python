4ab17dace8d6be850490bbd29cfe2320c495e5e4
cauacozz11 <cauacozz@gmail.com>
2025-05-20 09:34:48 -0300

    save

--- Trabalho phyton/historia.py
+++ Trabalho phyton/historia.py
@@ -5,10 +5,14 @@ def Mago():
     life = 4
     danger = 5
     info = f"Vida = {life}\n Ataque{5}"
-    return 
+    return life, danger, info
     
 
-
+def guerreiro():
+        life = 5
+        danger = 4
+        info = f"Vida = {life}\n Ataque{5}"
+        return info 
 
 
 
@@ -29,8 +33,7 @@ limparTELA()
 print("Escolha seu personagem para continuar sua jornada\n")
 
 escolha = ""
-
-while escolha not in ["mago", "guerreiro"]:
+while escolha not in[1,2]:
 
     print("Mago da Floresta de Nack")
     print("Nascido entre raízes encantadas, o mago canaliza a energia da natureza.")
@@ -41,13 +44,13 @@ while escolha not in ["mago", "guerreiro"]:
     print("Vida: 5 | Ataque: 4 | Especial: Fincada de Carneiro (1 a 4 de dano)\n")
     
 
-    escolha = input("Digite 'mago' ou 'guerreiro' para escolher seu herói: ").strip().lower()
+    escolha = int(input("Digite '1' ou '2' para escolher seu herói: "))
     limparTELA()
-    if escolha == "mago":
+if escolha == 1:
         print("Você escolheu o Mago da Floresta de Nack. Que os espíritos antigos guiem seus passos!\n")
-    elif escolha == "guerreiro":
+elif escolha == 2:
         print("Você escolheu o Guerreiro do Vulcão Legondi. Que sua espada arda como a lava!\n")
-    else:
+else:
       limparTELA() 
                  
 
@@ -58,23 +61,23 @@ limparTELA()
 
 # Escolha do primeiro destino
 local = ""
-while local not in["canyon", "vilarejo","libertália","libertalia"]:
+while local not in[1,2,3]:
     print("\nAgora, sua jornada começa. Você deve escolher para onde ir: \n")
     print("1. Canyon Cozz: lugar árido e perigoso, onde um ninja sombrio protege segredos antigos.")
     print("2. Vilarejo de Ludwig: ruínas silenciosas, dominadas pelo Guardião Caído.")
     print("3. Império Libertália: terra devastada e corrompida por Sephiroth. Um destino mortal.\n")
 
-    local  = input("Digite 'canyon' para ir ao Canyon Cozz, digite 'vilarejo' se deseja que seu destino seja o Vilarejo destruído de Ludwig ou digite 'libertália' se deseja ir ao reino destruído...Leve em consideração que a probabilidade de vitória é muito pequena: ").strip().lower()
+    local  = int(input("Digite '1' para ir ao Canyon Cozz, digite '2' se deseja que seu destino seja o Vilarejo destruído de Ludwig ou digite '3' se deseja ir ao reino destruído...Leve em consideração que a probabilidade de vitória é muito pequena: "))
     
     limparTELA()
 
-    if local == "canyon":
+if local == 1:
         print("Você decide seguir para o Canyon Cozz, onde mistérios aguardam e o ambiente desolado esconde segredos antigos.\n")
-    elif local == "vilarejo": 
+elif local == 2: 
         print("Você decide ir ao Vilarejo de Ludwig, onde os ecos de uma cidade destruída chamam por respostas que podem mudar tudo.\n")
-    elif local == "libertália":
+elif local == 3:
         print("Você decide seguir para Libertália, um reino em ruínas tomado pela escuridão. Algo ali é forte demais — você pareceque não estar pronto... mas que a sorte esteja com você.\n")
-    else:
+else:
         print("esse local não existe")
         limparTELA()
        
@@ -82,10 +85,42 @@ input("Pressione ENTER para começar sua viagem\n")
     
 limparTELA()
 
-if local == "canyon":
-        print("\nO caminho até o Canyon Cozz começa sutil, entre campos ressecados e árvores retorcidas pelo calor. Aos poucos, a vegetação desaparece, substituída por colinas áridas, onde o vento levanta poeira como se varresse os restos de algo esquecido. Você passa por restos de construções tomadas pela areia, pilares caídos e símbolos esculpidos em pedra que ninguém mais parece lembrar. Mais adiante, surgem formações rochosas afiadas, como lanças cravadas no chão por mãos colossais. A cada passo, o silêncio aumenta — e o horizonte começa a se abrir num corte profundo: o início do canyon. O ar ali parece mais pesado, como se algo observasse de longe... esperando.")
-elif  local == "vilarejo":
+if local == 1:
+        print("\nO caminho até o Canyon Cozz começa sutil, entre campos ressecados e árvores retorcidas pelo calor. Aos poucos, a vegetação desaparece, substituída por colinas áridas, onde o vento levanta poeira como se varresse os restos de algo esquecido. Você passa por restos de construções tomadas pela areia, pilares caídos e símbolos esculpidos em pedra que ninguém mais parece lembrar. Mais adiante, surgem formações rochosas afiadas, como lanças cravadas no chão por mãos colossais. A cada passo, o silêncio aumenta — e o horizonte começa a se abrir num corte profundo: o início do canyon. O ar ali parece mais pesado, como se algo observasse de longe... esperando.\n")
+        input("Pressione ENTER para continuar")
+        
+        limparTELA()
+        
+        print ("Você achou ao canyon, um lugar onde o silêncio domina e o misterio reina...\n")
+        input("Pressione ENTER para continuar")
+        
+        limparTELA()
+        
+        print("Começa a percebr uma movimentação estranha, e o silêncio que estava presentre a todo momento desaparece...\n")
+        print("FWWWWIP-TCHK", "uma Shuriken vem voando em sua direção e quase o atinge.")
+        input("Pressione ENTER para continuar")
+        
+        limparTELA()
+        
+        print("Você se esconde atrás de uma pedra e onserve quem está aproximando... A silhueta do ser vai tomando forma a cada passo que ele da, e de repente você percebe que stá diante de um Ninja do Canyon, um protetor das terras silencosas.")
+        print("Você precisa tomar uma decisão rapidamente.") 
+        input("Pressione ENTER para")
+        
+        limparTELA()
+        
+        escolha2 = int(input("Aperte 1 para lutar, ou aperte 2 para fugir:"))
+        if escolha2 == 1:
+                print("A batalha começa e você está com o ", Mago())
+                Mago()
+        elif escolha2 == 2:
+                print("A batalha começa e você está com o ", guerreiro())
+                guerreiro()        
+                
+         
+elif  local == 2:
         print("\nO caminho até Ludwig é envolto por névoa e silêncio. Árvores secas cercam a trilha, e pequenas ruínas esquecidas surgem entre as sombras. Cruzes tortas e casas despedaçadas anunciam o fim de algo que já foi vivo. No horizonte, envolto em bruma, o vilarejo aparece — quieto, quebrado, e à espera.") 
-elif local == "libertalia":
-        print ("\nO caminho até Libertália é sombrio e traiçoeiro. A estrada parece se desfazer sob seus pés, tomada por rachaduras, neblina e ecos de batalhas antigas. Estruturas quebradas surgem entre colinas escuras, como fantasmas de um reino que desabou sobre si mesmo. O ar é pesado, difícil de respirar — como se o próprio mundo avisasse que você não está pronto. E, ao longe, envolta em trevas, Libertália o observa. A chance de vitória é mínima... mas ainda assim, você continua.")             
+elif local == 3:
+        print ("\nO caminho até Libertália é sombrio e traiçoeiro. A estrada parece se desfazer sob seus pés, tomada por rachaduras, neblina e ecos de batalhas antigas. Estruturas quebradas surgem entre colinas escuras, como fantasmas de um reino que desabou sobre si mesmo. O ar é pesado, difícil de respirar — como se o próprio mundo avisasse que você não está pronto. E, ao longe, envolta em trevas, Libertália o observa. A chance de vitória é mínima... mas ainda assim, você continua.")   
+        
+                  
         
